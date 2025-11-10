2668dcdf61613573883b376dfbaf2aef55c4f29b
LauraEC <lauraescuderocabria@gmail.com>
2025-11-10 10:08:29 +0100

    Estructura de paquetes

--- src/main/java/com/example/messagingstompwebsocket/controller/GreetingController.java
+++ src/main/java/com/example/messagingstompwebsocket/controller/GreetingController.java
@@ -1,9 +1,12 @@
-package com.example.messagingstompwebsocket;
+package com.example.messagingstompwebsocket.controller;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
+
+import com.example.messagingstompwebsocket.model.Greeting;
+import com.example.messagingstompwebsocket.model.HelloMessage;
 
 @Controller
 public class GreetingController {
